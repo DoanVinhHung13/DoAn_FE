13412bb6aee93e72ab5c74c977d8a09bf05078d5
Đoàn Vĩnh Hưng <hungdoan1308@gmail.com>
2026-07-27 18:28:22 +0700

    Delete unused File

--- src/components/StepsCustom/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Steps } from "antd"
-
-const StepsCustom = ({ children, ...rest }) => {
-  return <Steps {...rest}>{children}</Steps>
-}
-
-export default StepsCustom
--- src/services/RoleService/urls.js
+++
@@ -1,9 +0,0 @@
-// src/services/RoleService/urls.js
-const URL_ROLE = {
-  GET_LIST_TAB: "/Role/GetListTab",
-  GET_LIST:     "/Role/GetList",
-  CREATE:       "/Role/Create",
-  UPDATE:       "/Role/Update",
-  DELETE:       "/Role/Delete",
-}
-export default URL_ROLE
